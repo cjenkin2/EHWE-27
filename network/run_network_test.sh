0f28ea0303c9d00893fd58fef67122d1c57f2a20
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 11:30:18 -0500

    Output redirection in scripts not what I expected it to be, making everything print to stdout/stderr

--- network/run_network_test.sh
+++ network/run_network_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo service network-manager stop
+sudo service network-manager stop 
 sudo killall -9 wpa_supplicant
-sudo wpa_supplicant -Dwext -iwlan0 -c wpa.conf -d &> ./network_test.log &
+sudo wpa_supplicant -B -Dwext -iwlan0 -c wpa.conf -d 
 sudo dhclient wlan0
