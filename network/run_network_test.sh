71e47e5479f6e192514cb8e28b1559ce600c0862
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 13:38:30 -0500

    Network test now tracks number of successful packets sent

--- network/run_network_test.sh
+++ network/run_network_test.sh
@@ -17,6 +17,20 @@ sudo dhclient wlan0
 # a network connection is established
 
 #Run network test commands
-ping -c 100 google.com &
-sudo ping -f genesi-tech.com &
 wget -b http://www.ubuntu.com/start-download?distro=desktop&bits=32&release=lts &
+
+#ping test
+#attribution: www.cyberciti.biz/tips/simple-linux-and-unix-system-monitoring-with-ping-command-and-scripts.html
+HOSTS="this_is_not_a_host genesi-tech.com google.com apple.com"
+DEADLINE=90
+
+for myHost in $HOSTS
+do
+	count=$(sudo ping -i 0 -w $DEADLINE $myHost | grep 'received' | awk -F',' '{ print $2v}' | awk '{ print $1 }')
+	echo "Count is $count"
+	if [ "$count" -eq 0 ]; then
+		echo "Could not reach host $myHost"
+	fi
+done
+#ping -c 100 google.com &
+#sudo ping -f genesi-tech.com &
