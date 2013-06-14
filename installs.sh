b5be2720cfbb48358e79d38f448d1397ca605a3b
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 13:13:05 -0500

    Added more required libraries to run test in installing script

--- installs.sh
+++ installs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-#install necessary graphical demos
+#install necessary packages for GPU tests
 sudo apt-get install libopenvg1-imx-demos libgles1-imx-demos libgles2-imx-demos
+
+#install necessary packages to run VPU tests
+sudo apt-get install gstreamer-tools0.10 graphviz
