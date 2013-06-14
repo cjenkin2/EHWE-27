b7858e5267d0738ce600f475260e5fa5339681e3
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 12:35:19 -0500

    Configured script to take conf file as argument. Code connects to internet via command line, uses ping and wget to test connectivity/cause keep network devices busy

--- network/run_network_test.sh
+++ network/run_network_test.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 1 ] #Expects one file - input video file 
+then
+        echo "Usage: $0 <wpa_configuration_file>"
+	echo "Note: use \`wpa_passphrase [ssid] [passphrase] > wpa.conf' to generate"
+        exit 65 # bad arguments
+fi
+
+#Start the network from the command line
 sudo service network-manager stop 
 sudo killall -9 wpa_supplicant
-sudo wpa_supplicant -B -Dwext -iwlan0 -c wpa.conf -d 
+sudo wpa_supplicant -B -Dwext -iwlan0 -c $1 -d 
 sudo dhclient wlan0
+
+#TODO query for status. Program marches on to tests whether or not
+# a network connection is established
+
+#Run network test commands
+ping -c 100 genesi-tech.com &
+sudo ping -f localhost &
+wget -b http://www.ubuntu.com/start-download?distro=desktop&bits=32&release=lts &
